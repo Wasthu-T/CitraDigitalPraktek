829a24e6e203cb6927b4fe845a24533357f31332
wasthu-t <wasthutatya55@gmail.com>
2024-09-27 14:34:02 +0700

    Pertemuan 2

---
+++ Praktik1/MenampilkanCitra.py
@@ -0,0 +1,45 @@
+# cv=computer vision
+import cv2
+import numpy as np
+
+# Membaca citra digital dari direktori
+citra = cv2.imread("Praktik1/Pohon.jpg")
+
+# Menampilkan gambar dari variabel citra
+# cv2.imshow("Pohon", citra)
+
+# Memisahkan channel warna
+blue = citra[:,:,0]
+green = citra[:,:,1]
+red = citra[:,:,2]
+# cv2.imshow("Pohon - Blue", blue)
+# cv2.imshow("Pohon - Green", green)
+# cv2.imshow("Pohon - Red", red)
+
+# Rumus Grayscale 0.299 R + 0.587 G + 0.114 B
+#==== Cara 1 Start ====#
+gray = np.around(0.299*red + 0.587*green + 0.114*blue)
+
+#==== Cara 1 End ====#
+
+#==== Cara 2 ====#
+# baris x kolom
+# baris = len(citra)
+# kolom = len(citra[0])
+
+# citra baru dengan array 0
+# gray = np.zeros((baris,kolom))
+
+# mengubah ke grayscale
+# for b in range(baris) :
+#     for k in range(kolom) :
+#         gray[b,k] = round(0.299*red[b,k] + 0.587*green[b,k] + 0.114*blue[b,k])
+
+#==== Cara 2 End====#
+
+# menghilangkan koma
+gray = gray.astype(np.uint8)
+cv2.imshow("Pohon - gray", gray)
+
+# Menunggu user menekan sembarang tombol
+cv2.waitKey()
---
+++ Praktik1/matrik.py
@@ -0,0 +1,6 @@
+import numpy as np
+matrik1 = np.matrix([[1,2,3,4],[5,6,7,8]])
+matrik2 = np.matrix([[1,2,3,4],[5,6,7,8]])
+matrik3 = np.matrix([[1,2,3,4],[5,6,7,8]])
+print(matrik1*1.5 + matrik2*2)
+# print(matrik[0,:])
