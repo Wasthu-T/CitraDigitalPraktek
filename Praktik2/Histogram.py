720f3a71d55f8b686db8c2c36a78d55b3b485594
wasthu-t <wasthutatya55@gmail.com>
2024-10-11 10:01:11 +0700

    update

--- Praktik2/Histogram.py
+++ Praktik2/Histogram.py
@@ -1,1 +1,34 @@
-print(1+1)
+# Histogram
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+citra = cv2.imread("asset/Pohon1.jpg")
+
+# conversi ke gray
+citra_gray = cv2.cvtColor(citra, cv2.COLOR_RGB2GRAY)
+
+# menyimpan jumlah baris dan jumlah kolom 
+jum_baris = len(citra)
+jum_kolom = len(citra[0])
+total_pix = jum_kolom + jum_baris
+
+# menghitung nilai pixel grayscale
+hist_gray = np.zeros(256) #inisiasi histogram
+# membuat array kosong dengan 256
+
+for i in range(jum_baris) :
+    for j in range(jum_kolom) : 
+        pixel = int(citra_gray[i][j])
+        hist_gray[pixel] += 1 
+
+# menampilkan histogram
+plt.bar(range(256),hist_gray)
+plt.show()
+
+# tampilkan gray image
+gray = citra_gray.astype(np.uint8)
+cv2.imshow("Pohon - gray", gray)
+
+# Menunggu user menekan sembarang tombol
+cv2.waitKey()
